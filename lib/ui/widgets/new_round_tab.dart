bf4d1fcb111b953d1e7896c249980f56b4aa53c7
eloy98g <eloygomez132@gmail.com>
2021-03-05 11:31:09 +0100

    new round button style added

--- lib/ui/widgets/new_round_tab.dart
+++ lib/ui/widgets/new_round_tab.dart
@@ -29,82 +29,11 @@ class _RoundEditTab extends State<RoundEditTab> {
       String aux;
       final int roundCount = widget.numRounds + 1;
 
-      final scoreRow = <Widget>[];
       final String _id = null;
       List<int> newResults = List();
       for (int i = 0; i < players.length; i++) {
         newResults.add(0);
       }
-
-      for (var i = 0; i < players.length; i++) {
-        scoreRow.add(new Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              players[i],
-              style: TextStyle(color: borderButton, fontSize: 16),
-            ),
-            Separator(width: 40),
-            Container(
-              width: 100,
-              height: 32,
-              decoration: BoxDecoration(
-                color: textPrimaryColor,
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Expanded(
-                  child: TextField(
-                keyboardType: TextInputType.number,
-                onChanged: (text) {
-                  setState(() async {
-                    aux = text;
-                    newResults[i] = int.parse(aux);
-                  });
-                },
-              )),
-            )
-          ],
-        ));
-        scoreRow.add(Separator(height: 8));
-      }
-      scoreRow.add(Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          RaisedButton(
-            color: textPrimaryColor,
-            child: Text(
-              'Cancelar',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-          Separator(width: 12),
-          RaisedButton(
-            color: textPrimaryColor,
-            child: Text(
-              'Aceptar',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-            onPressed: () async {
-              repository
-                  .appendRound(
-                    id: _id,
-                    date: DateTime.now(),
-                    matchId: widget.data.id,
-                    index: roundCount,
-                    results: newResults,
-                  )
-                  .then((value) => Navigator.of(context).pop());
-            },
-          )
-        ],
-      ));
 
       return AlertDialog(
         backgroundColor: backgroundThirdColor,
@@ -138,11 +67,126 @@ class _RoundEditTab extends State<RoundEditTab> {
             child: Container(
                 width: 273,
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  // crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Column(
-                      children: scoreRow,
+                    Expanded(
+                      child: Column(
+                        children: [
+                          ListView.separated(
+                            shrinkWrap: true,
+                            itemCount: players.length,
+                            separatorBuilder: (_, __) => Separator(height: 8),
+                            itemBuilder: (_, index) {
+                              return Container(
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      players[index],
+                                      style: TextStyle(
+                                          color: borderButton, fontSize: 16),
+                                    ),
+                                    Separator(width: 40),
+                                    Container(
+                                      width: 100,
+                                      height: 32,
+                                      decoration: BoxDecoration(
+                                        color: textPrimaryColor,
+                                        borderRadius: BorderRadius.circular(8),
+                                      ),
+                                      child: Expanded(
+                                          child: TextField(
+                                        keyboardType: TextInputType.number,
+                                        onChanged: (text) {
+                                          setState(() async {
+                                            aux = text;
+                                            newResults[index] = int.parse(aux);
+                                          });
+                                        },
+                                      )),
+                                    )
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                          Separator(height: 24),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              InkWell(
+                                child: Container(
+                                    width: 100,
+                                    height: 33,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      border: Border.all(color: borderButton),
+                                      color: backgroundThirdColor,
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.5),
+                                          spreadRadius: 0,
+                                          blurRadius: 4,
+                                          offset: Offset(2, 4),
+                                        ),
+                                      ],
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'Cancelar',
+                                        style: TextStyle(
+                                            color: textPrimaryColor,
+                                            fontSize: 16),
+                                      ),
+                                    )),
+                                onTap: () => Navigator.of(context).pop(),
+                              ),
+                              Separator(width: 12),
+                              InkWell(
+                                child: Container(
+                                    width: 100,
+                                    height: 33,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(20),
+                                      border: Border.all(color: borderButton),
+                                      color: backgroundThirdColor,
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black.withOpacity(0.5),
+                                          spreadRadius: 0,
+                                          blurRadius: 4,
+                                          offset: Offset(2, 4),
+                                        ),
+                                      ],
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'Aceptar',
+                                        style: TextStyle(
+                                            color: textPrimaryColor,
+                                            fontSize: 16),
+                                      ),
+                                    )),
+                                onTap: () async {
+                                  repository
+                                      .appendRound(
+                                        id: _id,
+                                        date: DateTime.now(),
+                                        matchId: widget.data.id,
+                                        index: roundCount,
+                                        results: newResults,
+                                      )
+                                      .then((value) =>
+                                          Navigator.of(context).pop());
+                                },
+                              )
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ],
                 )),
